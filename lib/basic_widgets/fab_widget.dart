e9b0f859f844c103852bcf2218d9355be0cac941
RIZKYANGKATA <rizmadoris@gmail.com>
2023-10-05 10:37:13 +0700

    update pertemuan 5

---
+++ lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FabWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
